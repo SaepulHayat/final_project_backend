e2428ad783454458c0436dc64f505d3b01b44361
Erland Maheswara <iammahesvara@gmail.com>
2025-04-27 01:32:41 +0700

    Base

--- src/app/model/__init__.py
+++ src/app/model/__init__.py
@@ -1,23 +1,5 @@
 from .user import User
-from .book import Book
-from .author import Author
-from .book_author import BookAuthor
-from .genre import Genre
-from .book_genre import BookGenre
-from .publisher import Publisher
-from .store import Store
-from .product_listing import ProductListing
-from .review import Review
 
 __all__ = [
     'User',
-    'Book',
-    'Author',
-    'BookAuthor',
-    'Genre',
-    'BookGenre',
-    'Publisher',
-    'Store',
-    'ProductListing',
-    'Review',
 ]
